07b6b57fc177f15de71aadf177029bcecebb683e
RahulRajesh2001 <rahulrajesh2001239@gmail.com>
2024-02-21 15:37:12 +0530

    Add login frontend validation with formik hook

--- user/src/components/signin/SignIn.jsx
+++ user/src/components/signin/SignIn.jsx
@@ -1,63 +1,106 @@
 import React, { useState } from 'react'
 import FilledButton from '../../components/buttons/filledbutton/FilledButton'
 import Google from '../../assets/Google.png'
-import axios from 'axios';
-import {baseUrl} from '../../../baseUrl.js'
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import {setUser} from '../../../redux/reducers/userSlice.js'
-
+import axios from 'axios'
+import { baseUrl } from '../../../baseUrl.js'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { setUser } from '../../../redux/reducers/userSlice.js'
+import { useFormik } from 'formik'
+import { loginSchema } from '../../formValidationSchema/loginFormValidation.js'
 
 const SignIn = () => {
-  const navigate=useNavigate()
-  const dispatch=useDispatch()
-  const [email,setEmail]=useState('rahulrjev@gmail.com');
-  const [password,setPassword]=useState("12345")
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-  const user={
-    email,
-    password
-  }
-
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    try{
-      axios.post(`${baseUrl}/api/v1/login`,user).then((response)=>{
+  const onSubmit = (values, actions) => {
+    const user = {
+      email: values.email,
+      password: values.password,
+    }
+    try {
+      axios.post(`${baseUrl}/api/v1/login`, user).then((response) => {
         console.log(response)
         dispatch(setUser(response.data))
         navigate('/')
 
         alert(response.data.message)
       })
-    }catch(error){
+    } catch (error) {
       console.log(error)
       navigate('/loginSignup')
     }
+
+    actions.resetForm()
   }
+
+  //formik validation
+  const {
+    values,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    errors,
+    isSubmitting,
+  } = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: loginSchema,
+    onSubmit,
+  })
+
+  console.log(errors)
 
   return (
     <div>
       {/*signinbody*/}
       <div className='flex flex-col justify-center ml-[20px] mt-[10px] w-[90%] gap-2'>
-        {/*input box*/}
-        <div className='flex  flex-col gap-2 '>
-          <div className='text-[12px] font-semibold '>Email Address</div>
-          <input type='email' className='border outline-none  h-[35px]' onChange={(e)=>setEmail(e.target.value)}/>
-        </div>
-        {/*input box*/}
-        <div className='flex  flex-col gap-2 '>
-          <div className='flex justify-between'>
-            <div className='text-[12px] font-semibold '>Password</div>
-            <div className='text-[12px] font-semibold text-[#2DA5F3]'>
-              Forget Password
-            </div>
+        {/*From validation*/}
+        <form className='flex justify-center flex-col' onSubmit={handleSubmit}>
+          {/*input box*/}
+          <div className='flex  flex-col gap-2 '>
+            <div className='text-[12px] font-semibold '>Email Address</div>
+            <input
+              name='email'
+              type='email'
+              className={`border h-[35px]  ${
+                errors.email ? 'outline-red-400 ' : 'outline-none'
+              }`}
+              value={values.email}
+              onChange={handleChange}
+            />
+            {errors.email ? <p className='text-[10px] '>{errors.email}</p> : ''}
           </div>
-          <input type='password' className='border outline-none  h-[35px]' onChange={(e)=>setPassword(e.target.value)}/>
-        </div>
-        <div className='mt-2' onClick={handleSubmit}>
-          <FilledButton value='SIGN IN' w='100%'/>
-        </div>
-
+          {/*input box*/}
+          <div className='flex  flex-col gap-2 '>
+            <div className='flex justify-between'>
+              <div className='text-[12px] font-semibold '>Password</div>
+              <div className='text-[12px] font-semibold text-[#2DA5F3]'>
+                Forget Password
+              </div>
+            </div>
+            <input
+              name='password'
+              type='password'
+              className={`border h-[35px]  ${
+                errors.password ? 'outline-red-400 ' : 'outline-none'
+              }`}
+              value={values.password}
+              onChange={handleChange}
+            />
+            {errors.password ? (
+              <p className='text-[10px] '>{errors.password}</p>
+            ) : (
+              ''
+            )}
+          </div>
+          <button className='mt-2' type='submit' disabled={isSubmitting}>
+            <FilledButton value='SIGN IN' w='100%' />
+          </button>
+        </form>
+        {/*0auth section*/}
         <div className='h-[1px] bg-[#E4E7E9] mt-2'></div>
         {/*O Auth*/}
         <div className='h-[35px] border border-[#E4E7E9] flex  items-center mt-5'>
---
+++ user/src/formValidationSchema/loginFormValidation.js
@@ -0,0 +1,8 @@
+import * as yup from 'yup'
+
+const passwordRules=/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z]).{8,}$/
+
+export const loginSchema=yup.object().shape({
+    email:yup.string().email("Please enter valid email!").required("Required!"),
+    password:yup.string().min(8).matches(passwordRules,{message:"Please create a stronger password!"}).required("Required!")
+})
