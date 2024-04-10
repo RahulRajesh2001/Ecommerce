6cfac75e81986acb158301419427833f5fb221db
RahulRajesh2001 <rahulrajesh2001239@gmail.com>
2024-04-10 12:07:46 +0530

    Mongodb connected to atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose'
 
 export function connectDB() {
+  const password=process.env.MONGODB_PASSWORD;
   mongoose
-    .connect('mongodb://127.0.0.1:27017/Quilon')
+    .connect(`mongodb+srv://rahulrjev:${password}@neomcluster.hklzlwe.mongodb.net/?retryWrites=true&w=majority&appName=NeomCluster`)
     .then(() => {
       console.log('MongoDB connected..!')
     })
